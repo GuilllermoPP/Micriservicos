d1908b7806b40d5708e594d5fd64f407493d3fa5
GuilllermoPP <142278424+GuilllermoPP@users.noreply.github.com>
2024-03-06 20:52:21 -0500

    Add files via upload

---
+++ entidades/src/main/java/edu/unimagdalena/microg2/repository/SugerenciaRepository.java
@@ -0,0 +1,9 @@
+package edu.unimagdalena.microg2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import edu.unimagdalena.microg2.entities.Sugerencia;
+
+public interface SugerenciaRepository extends JpaRepository <Sugerencia, Long >{
+    
+}
